13126d2d32cc812e00819da0fd29061e3867196e
ayoub <ayoub.melle@gmail.com>
2018-12-09 19:27:39 +0100

    constructeur

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,4 +1,41 @@
 package image;
+import javafx.scene.paint.Color;
 
-public class BruteRasterImage {
+
+public class BruteRasterImage implements Image {
+    int width;
+    int height;
+    Color color;
+    Image MyImage;
+
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+
+
+    public BruteRasterImage(Color color, int width, int height)
+    {
+
+        this.width=width;
+        this.height=height;
+        this.color=color;
+
+    }
+
+
+
+
 }

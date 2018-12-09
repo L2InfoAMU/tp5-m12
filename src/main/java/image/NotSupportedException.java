57836b0a5019822fc21cab5c09f210fd5bbe8147
ayoub <ayoub.melle@gmail.com>
2018-12-09 20:46:15 +0100

    create Notsupportted exception class

---
+++ src/main/java/image/NotSupportedException.java
@@ -0,0 +1,10 @@
+package image;
+
+public class NotSupportedException  extends  RuntimeException{
+    String string;
+
+    public NotSupportedException(String message, String string) {
+        super(message);
+        this.string = string;
+    }
+}
